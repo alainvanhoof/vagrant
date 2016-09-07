5db17c07d97488cc345627c9026bf875329f43b2
Alain van Hoof <alain@lafeberhof.nl>
2016-09-08 00:15:46 +0200

    Added PostgreSQL database

---
+++ CentOS7/Postgresql/Provision.sh
@@ -0,0 +1,18 @@
+# EPEL and Update
+#yum -y install epel-release
+#yum -y update
+
+cat >> /etc/yum.repos.d/postgresql94.repo << EOF
+[postgresql95]
+name=PostgreSQL 9.5 CentoOS 7
+baseurl=https://download.postgresql.org/pub/repos/yum/9.5/redhat/rhel-7-x86_64
+gpgcheck=0
+enabled=1
+EOF
+
+yum -y install postgresql-server
+
+postgresql-setup initdb
+
+systemctl enable postgresql
+systemctl start postgresql
