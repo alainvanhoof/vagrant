4ca9879dee9a069064a1630a940d9ae4a32ea862
Alain van Hoof <alain@lafeberhof.nl>
2017-05-03 00:04:26 +0200

    Improved startup stuff

---
+++ Alpine/Rocket.Chat/roles/alpine_rocketchat/files/rocketchat.conf.d
@@ -0,0 +1,2 @@
+ROCKETCHAT_OPTS="main.js"
+ROCKETCHAT_ENV="env PORT=3000 ROOT_URL=http://localhost:3000/ MONGO_URL=mongodb://localhost:27017/rocketchat"
---
+++ Alpine/Rocket.Chat/roles/alpine_rocketchat/files/rocketchat.init.d
@@ -0,0 +1,31 @@
+#!/sbin/openrc-run
+
+USER=root
+SERVICE="rocketchat"
+COMMAND="/usr/bin/node"
+CMD_ARGS=${ROCKETCHAT_OPTS}
+CMD_ENV=${ROCKETCHAT_ENV}
+DIR="/opt/rocketchat/bundle"
+
+depend() {
+    need net
+    need localmount
+}
+
+start() {
+    ebegin "Starting ${SERVICE}"
+    start-stop-daemon --background --start \
+    --make-pidfile --pidfile /run/${SERVICE}.pid \
+    --exec ${CMD_ENV} ${COMMAND} \
+    --user ${USER:-root} \
+    --chdir ${DIR} \
+    -- ${CMD_ARGS}
+    eend $?
+}
+
+stop() {
+    ebegin "Stopping ${SERVICE}"
+    start-stop-daemon --stop --exec ${COMMAND} \
+    --pidfile /run/${SERVICE}.pid
+    eend $?
+}
