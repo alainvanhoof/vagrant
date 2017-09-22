5aaf0b309c208bd91c0ddbe40f1830eda0715a0c
Alain van Hoof <alain@lafeberhof.nl>
2017-09-22 20:21:36 +0200

    Changes IP and IP of ES to connect to

--- UpdateVagrant.bash
+++ UpdateVagrant.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-VA_VERSION=1.9.4
+VA_VERSION=2.0.0
 curl -O https://releases.hashicorp.com/vagrant/${VA_VERSION}/vagrant_${VA_VERSION}_x86_64.dmg
 hdiutil attach ./vagrant_${VA_VERSION}_x86_64.dmg
 sudo installer -pkg /Volumes/Vagrant/Vagrant.pkg -target /Volumes/Macintosh\ HD
