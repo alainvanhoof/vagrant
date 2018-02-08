15aedd002994614944e21cdcb1d20d286dda3022
Alain van Hoof <alain@lafeberhof.nl>
2018-02-08 22:16:30 +0100

    Version bump

--- UpdateVagrant.bash
+++ UpdateVagrant.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-VA_VERSION=2.0.0
+VA_VERSION=2.0.2
 curl -O https://releases.hashicorp.com/vagrant/${VA_VERSION}/vagrant_${VA_VERSION}_x86_64.dmg
 hdiutil attach ./vagrant_${VA_VERSION}_x86_64.dmg
 sudo installer -pkg /Volumes/Vagrant/Vagrant.pkg -target /Volumes/Macintosh\ HD
