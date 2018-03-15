8170db33d9a3719e6dc7ad703866d83b06c02188
Alain van Hoof <alain@lafeberhof.nl>
2018-03-16 00:23:54 +0100

    Initial Commit

---
+++ CentOS7/Lustre/ProvisionClient.sh
@@ -0,0 +1,32 @@
+# EPEL and Update
+# yum -y install epel-release
+yum -y update
+
+# Disable SELinux
+setenforce 0
+sed -i -e "s/SELINUX=enforcing/SELINUX=disabled/"  /etc/sysconfig/selinux
+
+# Create YUM Repo
+
+cat > /etc/yum.repos.d/lustre.repo << EOF
+[hpddLustreclient]
+name=CentOS-\$releasever - Lustre
+baseurl=http://build.hpdd.intel.com/job/lustre-master/arch=x86_64%2Cbuild_type=client%2Cdistro=el7%2Cib_stack=inkernel/lastSuccessfulBuild/artifact/artifacts/
+gpgcheck=0
+EOF
+
+sed -i -e "/lustre/d" /etc/hosts
+
+cat >> /etc/hosts << EOF
+# Lustre
+10.0.0.201 lustre01 
+10.0.0.202 lustre02
+10.0.0.203 lustre03
+EOF
+
+cat >> /etc/modprobe.d/lustre.conf << EOF
+options lnet networks=tcp(eth1)
+EOF
+
+yum -y install lustre-client
+reboot
